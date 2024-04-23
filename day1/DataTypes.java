f316c5feb2ad300c8e77e4923dcec79600994729
BhavaniGokul <167833166+BhavaniGokul@users.noreply.github.com>
2024-04-23 14:06:14 +1200

    Add files via upload
    Day1

---
+++ day1/DataTypes.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int tabOpened=3;
+		float verion=132.45F;
+		char firstLetter='C';
+		boolean loginSucsful=true;
+		String browserName="Chrome";
+		
+		System.out.println("Number of tabs opened: "+tabOpened+"\n"+"Version of the browser: "+verion);
+		System.out.println("First letter of the browser: "+firstLetter+"\n"+"Name of the browser: "+browserName);
+	}
+}
